25feffea5ee85a457cbbaf4356f230616b2ed173
parksoli <baksoli@genieworks.net>
2021-02-24 16:30:29 +0900

    springBoot Start

--- src/main/java/com/shop/quick/QuickApplication.java
+++ src/main/java/com/shop/quick/QuickApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("welcome to spring");
 		SpringApplication.run(QuickApplication.class, args);
 	}
 
